83dd88ce556a5cc2bb26d6ec75d7d759daf80c97
bug41 <bugkingus@gmail.com>
2022-02-14 11:43:06 -0800

    최신화

--- src/index.js
+++ src/index.js
@@ -7,36 +7,34 @@ import faker from 'faker';
 const App = () => {
     return (
         <div className="ui container comments">    
-
             <ApprovalCard>
                 <div>
                     <h4>Warning!</h4>
                     Are You Sure you want to do this?
-
                 </div>
-               </ApprovalCard>
+            </ApprovalCard>
             <ApprovalCard>
                 <CommentDetail 
                     author="Sam"     
                     timeAgo="Today at 4:45PM" 
                     content="test1"
                     avatar={faker.image.image()}
-                    />
+                />
             </ApprovalCard>            
             <ApprovalCard>
                 <CommentDetail 
-                author="Nick"    
-                timeAgo="Today at 2:55PM" 
-                content="test2"
-                avatar={faker.image.image()}
+                    author="Nick"    
+                    timeAgo="Today at 2:55PM" 
+                    content="test2"
+                    avatar={faker.image.image()}
                 />
             </ApprovalCard>
             <ApprovalCard>
                 <CommentDetail 
-                author="Kelly"   
-                timeAgo="Yesterday at 5:55PM" 
-                content="test3"
-                avatar={faker.image.image()}
+                    author="Kelly"   
+                    timeAgo="Yesterday at 5:55PM" 
+                    content="test3"
+                    avatar={faker.image.image()}
                 />
             </ApprovalCard>
         </div>
